5aa1dc79838011c62cb102b3cb3c62428df41a3c
Igor Petrovic <758832+igorovic@users.noreply.github.com>
2024-04-14 11:43:31 +0200

    wip

--- confs/.zfunc/nodejs-dev.zsh
+++ confs/.zfunc/nodejs-dev.zsh
@@ -3,6 +3,8 @@ check_cmd() {
     command -v "$1" >/dev/null 2>&1
 }
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 install_node(){
     if ! check_cmd nvm; then
         /usr/bin/bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
@@ -19,6 +21,3 @@ install_node(){
 install_node
 # reset pnpm global store in container - to avoid conflicts with the host's store
 pnpm config set store-dir "${$(pnpm config get store-dir):-$HOME/.pnpm-store}" --global
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
